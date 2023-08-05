fcf6076869e6ab958894f6ae9576a298c2387dd4
Timothy White <bigtball321@yahoo.com>
2023-08-05 19:55:16 -0400

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,5 +1,6 @@
 // implement your posts router here
 const express = require('express')
+const Post = require('./posts-model')
 
 const router = express.Router();
 
@@ -15,22 +16,39 @@ router.get('/', (req, res) => {
                 stack: err.stack,
             })
         })
+ })
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            res.json(post)
+        }
+        
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack, 
+    })
+}
 })
-router.get('/:id', (req, res) => {
+// router.post('/', (req, res) => {
 
-})
-router.post('/', (req, res) => {
+// })
+// router.delete('/:id', (req, res) => {
 
-})
-router.delete('/:id', (req, res) => {
+// })
+// router.put('/:id', (req, res) => {
 
-})
-router.put('/:id', (req, res) => {
+// })
+// router.get('/:id/messages', (req, res) => {
 
-})
-router.get('/:id/messages', (req, res) => {
-
-})
+// })
 
 
 module.exports = router
