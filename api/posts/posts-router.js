dd754e477a3d0eef117741c2caff91dfee969363
Timothy White <bigtball321@yahoo.com>
2023-08-04 21:14:33 -0400

    codegrade is buggy rn

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,34 @@ const express = require('express')
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Post.find()
+        .then(found => {
+            res.json(found)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post information could not be retrieved",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
+router.get('/:id', (req, res) => {
+
+})
+router.post('/', (req, res) => {
+
+})
+router.delete('/:id', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+router.get('/:id/messages', (req, res) => {
+
+})
+
+
 module.exports = router
