2b363521ecba4caa19bc20dc4a81ac6fff4ac843
Timothy White <bigtball321@yahoo.com>
2023-08-05 20:59:05 -0400

    finished

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -116,25 +116,20 @@ router.put('/:id', (req, res) => {
         })
     }
 })
-router.get('/:id/messages', async (req, res) => {
+router.get('/:id/comments', async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        if (!post) {
-            res.status(404).json({
-                message: 'The post with the specified ID does not exist'
-            });
+        if (post) {
+            const comments = await Post.findPostComments(req.params.id);
+            res.json(comments);
         } else {
-            const messages = await Post.findPostComments(req.params.id);
-            res.json(messages);
+            res.status(404).json({ message: "The post with the specified ID does not exist" });
         }
-    } catch(err) {
-        res.status(500).json({
-            message: "The post information could not be retrieved",
-            err: err.message,
-            stack: err.stack,
-        });
+    } catch (err) {
+        res.status(500).json({ message: "The comments information could not be retrieved" });
     }
 });
+
 
 
 
